3df18f8c09087e0d6a4c8b1f21599dc7cd7e00e5
Orsouene <orsoueneelaouizeb@gmail.com>
2025-01-31 22:46:04 +0100

    Fatto il Footer

--- src/Components/Footer/FooterComponent.jsx
+++ src/Components/Footer/FooterComponent.jsx
@@ -1,7 +1,60 @@
 import React from "react";
-
+import styles from "../Footer/FooterComponent.module.css";
 function FooterComponent() {
-  return <div>FooterComponent</div>;
+  return (
+    <footer className={styles.footer}>
+      <div className={`${styles.footerLeft} ${styles.colMd4} ${styles.colSm6}`}>
+        <div className={styles.icons}>
+          <a href="#">
+            <i className="fa fa-facebook"></i>
+          </a>
+          <a href="#">
+            <i className="fa fa-twitter"></i>
+          </a>
+          <a href="#">
+            <i className="fa fa-linkedin"></i>
+          </a>
+          <a href="#">
+            <i className="fa fa-google-plus"></i>
+          </a>
+          <a href="#">
+            <i className="fa fa-instagram"></i>
+          </a>
+        </div>
+      </div>
+      <div
+        className={`${styles.footerCenter} ${styles.colMd4} ${styles.colSm6}`}
+      >
+        <div>
+          <i className="fa fa-map-marker"></i>
+          <p>
+            <span> indirizzo,</span> cità, paese
+          </p>
+        </div>
+        <div>
+          <i className="fa fa-phone"></i>
+          <p> (+00) 0000 000 000</p>
+        </div>
+        <div>
+          <i className="fa fa-envelope"></i>
+          <p>
+            <a href="#"> officio@agenzia.com</a>
+          </p>
+        </div>
+      </div>
+      <div
+        className={`${styles.footerRight} ${styles.colMd4} ${styles.colSm6}`}
+      >
+        <h2>
+          Agenzia<span> logo</span>
+        </h2>
+        <p className={styles.menu}>
+          <a href="#"> Servizi</a> |<a href="#"> Portfolio</a>
+        </p>
+        <p className={styles.name}> Nome agenzia &copy; 2025</p>
+      </div>
+    </footer>
+  );
 }
 
 export default FooterComponent;
