768612fc4296278dcbf70e20f56e593b9a7961d0
Orsouene <orsoueneelaouizeb@gmail.com>
2025-02-01 15:07:45 +0100

    Aggiornato Codice (2)

--- src/Components/Header/HeaderComponent.jsx
+++ src/Components/Header/HeaderComponent.jsx
@@ -22,41 +22,40 @@ function HeaderComponent() {
   }, []);
 
   return (
-    <div className={styles.jumbotron}>
-      <header className={`${styles.header} ${styles.FontHeaderFooter}`}>
-        <ul className={`${styles.menu} ${menuOpen ? styles.open : ""}`}>
-          <li>
-            <NavLink to="/about" className={styles.myLink}>
-              About
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/Contact" className={styles.myLink}>
-              Contact
-            </NavLink>
-          </li>
-        </ul>
-        <label htmlFor="menu-btn" className={styles.menuIcon}>
-          <span className={styles.navicon}></span>
-        </label>
-        <a href="/" className={styles.logo}>
-          Logo
-        </a>
-        <input
-          type="checkbox"
-          id="menu-btn"
-          className={styles.menuBtn}
-          checked={menuOpen}
-          onChange={toggleMenu}
-        />
-
+    <header className={`${styles.header} ${styles.FontHeaderFooter}`}>
+      <ul className={`${styles.menu} ${menuOpen ? styles.open : ""}`}>
+        <li>
+          <NavLink to="/about" className={styles.myLink}>
+            About
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/Contact" className={styles.myLink}>
+            Contact
+          </NavLink>
+        </li>
+      </ul>
+      <label htmlFor="menu-btn" className={styles.menuIcon}>
+        <span className={styles.navicon}></span>
+      </label>
+      <a href="/" className={styles.logo}>
+        Logo
+      </a>
+      <input
+        type="checkbox"
+        id="menu-btn"
+        className={styles.menuBtn}
+        checked={menuOpen}
+        onChange={toggleMenu}
+      />
+      <div className={styles.jumbotron}>
         <img
           src={images[currentImageIndex]}
           alt="Jumbotron"
           className={styles.image}
         />
-      </header>
-    </div>
+      </div>
+    </header>
   );
 }
 
